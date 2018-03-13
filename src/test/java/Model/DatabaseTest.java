de331fc28c86c0730b86a28e30cc504bf4bd2611
Teuber <michael@teuber.dk>
2018-03-13 09:22:39 +0100

    DB test
    JUnit for Database

--- src/test/java/Model/DatabaseTest.java
+++ src/test/java/Model/DatabaseTest.java
@@ -1,4 +1,22 @@
 package Model;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class DatabaseTest {
+
+        @Test
+        public void testDatabase() throws Exception {
+
+            Database datab = new Database();
+
+            boolean test1 = datab.execute("INSERT INTO movie (title, genre, price, length) " +
+                    "VALUES('test', 'test', 'test', 'test')");
+
+            boolean test2 = datab.execute("DELETE FROM movie WHERE title='test';");
+
+            Assert.assertTrue(test1);
+            Assert.assertTrue(test2);
+
+        }
 }
